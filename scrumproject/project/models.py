2c4022dba9f101967608e11d49a1eef1dbc6db1b
edewajosh <joshedewa@gmail.com>
2022-05-27 16:55:32 +0300

    added project features

--- scrumproject/project/models.py
+++ scrumproject/project/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=254)
+    date_started = models.DateTimeField(auto_now_add=True, null=False)
+    description = models.TextField()
+
+class Release(models.Model):
+
+    name = models.CharField(max_length=254)
+    project = models.ForeignKey(
+                                Project,
+                                blank=True,
+                                null=True
+                                on_delete=models.CASCADE
+                            )
+    release_date = models.DateTimeField(auto_now_add=True, null=False)
+    description = models.TextField()
+
+class Sprint(models.Model):
+    name = models.CharField(max_length=254)
+    starts_on = models.DateField()
+    ends_on = models.DateField()
+    project = models.ForeignKey(Project, verbose_name='project_assigned', on_delete=models.CASCADE)
+    release = models.ForeignKey(Release, 
+                                verbose_name='released_assigned',
+                                blank=True,
+                                null=True,
+                                on_delete=models.CASCADE)
