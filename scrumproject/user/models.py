1b3d04c82b60be60b15a869c200ceb3cc6a5ec8b
edewajosh <joshedewa@gmail.com>
2022-07-01 12:27:03 +0300

    User can now be updated with group/permissions

--- scrumproject/user/models.py
+++ scrumproject/user/models.py
@@ -42,4 +42,3 @@ class User(AbstractBaseUser, PermissionsMixin):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
-
--- scrumproject/user/serializers.py
+++ scrumproject/user/serializers.py
@@ -16,6 +16,21 @@ class UserSerializer(ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class UserPermissionsSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'is_admin', 'is_active', 'last_login', 'password', 'groups']
+        extra_kwargs = {
+            'password': {'write_only': True}, 
+            'last_login': {'read_only': True},
+            'is_active': {'read_only': True},
+            'is_admin': {'read_only': True},
+        }
+
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
         
 # Is a good design pattern to provide an endpoint for creating superusers?
 class AdminUserSerializer(ModelSerializer):
